7e554c91f4bc3242c52f578e57efc60c2415d04f
Glaucya <stobbeglaucya@gmail.com>
2021-12-20 07:54:23 -0300

    Resolução exercicio collections

---
+++ Estoque.java
@@ -0,0 +1,25 @@
+package br.com.generetion.collections;
+
+public class Estoque {
+
+	private String itens;
+
+	public String getItens() {
+		return itens;
+	}
+
+	public void setItens(String itens) {
+		this.itens = itens;
+	}
+
+	public Estoque(String itens) {
+		super();
+		this.itens = itens;	
+	}
+
+	
+	public String toString() {
+		return "Estoque [itens=" + itens + "]";
+	
+	}
+}
